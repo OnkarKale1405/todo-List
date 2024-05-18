2393038d31563563a2bf36282922a2921192992a
OnkarKale1405 <onkarkale0007@gmail.com>
2024-05-18 11:19:34 +0530

    development

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 const app = express();
 
@@ -16,6 +17,7 @@ app.use(express.urlencoded({
 app.use(express.static("public"));
 app.use(cookieParser())
 
+
 // routes import 
 import userRouter from "./routes/user.routes.js"
 import todoRouter from "./routes/todo.routes.js"
@@ -25,5 +27,21 @@ import subTodoRouter from "./routes/subTodo.routes.js"
 app.use("/v1/api/users", userRouter);
 app.use("/v1/api/todos", todoRouter);
 app.use("/v1/api/subTodos", subTodoRouter);
+
+// ------------ Development ------------------ //
+
+const __dirname1 = path.resolve();
+if(process.env.NODE_ENV){
+    app.use(express.static(path.join(__dirname1,'/frontend/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+    })
+} else {
+    app.get('/', (req, res) => {
+        res.send("API is running successfully");
+    })
+}
+
+// ------------ Development ------------------ //
 
 export { app };
